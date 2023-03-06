5b60f1819ea59ea08fd85d3028a0bb135e9cd06f
Souzan <Souzan@DESKTOP-VHKB7GI>
2023-03-06 14:40:58 +0200

    Add project files.

---
+++ Uppgift 4.9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv ett tal för sidlängden till triangeln");
+        int x = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i <= x; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
